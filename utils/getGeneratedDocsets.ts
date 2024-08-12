addb0fa5d5d12256261f3226bea33ec508b274b7
Santiago Montoya <xantiagoma@gmail.com>
2024-08-11 23:55:48 -0500

    Move URL to use Git LFS

--- utils/getGeneratedDocsets.ts
+++ utils/getGeneratedDocsets.ts
@@ -39,7 +39,9 @@ export async function getGeneratedDocsets(filterName?: string) {
 		object.name = key;
 		object.urls = [
 			// `https://cdn.jsdelivr.net/gh/DashDocsets/docsets@master/docsets/${key}.tgz`,
-			`https://raw.githubusercontent.com/DashDocsets/docsets/master/docsets/${key}.tgz`,
+			// `https://raw.githubusercontent.com/DashDocsets/docsets/master/docsets/${key}.tgz`,
+			// GIT LFS
+			`https://media.githubusercontent.com/media/DashDocsets/docsets/master/docsets/${key}.tgz`,
 		];
 
 		return object;
